4d652c0c36fbd1b8930c60a0cd8ba55e72759fba
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-22 20:31:25 +0200

    [UPD] fix millestone 1 and page style improvements

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
-        return{
+        return {
             todos: [
-                {text: "Quartely Newsletter", done: true},
-                {text: "Recruiting blog post", done: false},
-                {text: "Mobile app launch", done: true},
-                {text: "Interview John H.", done: true},
-                {text: "Summit update to mobile storefronts", done: false},
-                {text: "Schedule meeting with Alex", done: true},
-                {text: "Homepage refresh", done: false},
-                {text: "Onboard new Sales team members", done: false},
-                {text: "Review editorial calendar", done: true},
+                { text: "Quartely Newsletter", done: true },
+                { text: "Recruiting blog post", done: false },
+                { text: "Mobile app launch", done: true },
+                { text: "Interview John H.", done: true },
+                { text: "Summit update to mobile storefronts", done: false },
+                { text: "Schedule meeting with Alex", done: true },
+                { text: "Homepage refresh", done: false },
+                { text: "Onboard new Sales team members", done: false },
+                { text: "Review editorial calendar", done: true },
 
             ]
         };
     },
-    methods:{
-        
+    methods: {
+
         removeTask(index) {
             this.todos.splice(index, 1);
         },
-        
+        setTaskCompletion(todo) {
+            todo.done = !todo.done;
+        }
     }
 }).mount('#app')
