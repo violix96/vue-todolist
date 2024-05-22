1a9c95ffda96e8c12876aafa8e9f999f32117a1d
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-22 16:22:53 +0200

    [UPD] millestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("ciao mondo");
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return{
+            todos: [
+                {text: "Quartely Newsletter", done: true},
+                {text: "Recruiting blog post", done: false},
+                {text: "Mobile app launch", done: true},
+                {text: "Interview John H.", done: true},
+                {text: "Summit update to mobile storefronts", done: false},
+                {text: "Schedule meeting with Alex", done: true},
+                {text: "Homepage refresh", done: false},
+                {text: "Onboard new Sales team members", done: false},
+                {text: "Review editorial calendar", done: true},
+
+            ]
+        };
+    },
+    methods:{
+        removeTask(index) {
+            this.todos.splice(index, 1);
+        },
+    }
+}).mount('#app')
